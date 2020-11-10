a96a65db591eb7c974ebb2aab3b576f14f3024ec
Pete Haughie <pete.haughie@provartesting.com>
2020-11-10 10:36:47 +0100

    Updated barebones script

--- tasks/barebones.js
+++ tasks/barebones.js
@@ -1,6 +1,12 @@
 const { webkit } = require('playwright');
 
 (async () => {
+  const browser = await webkit.launch({
+    headless: false,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
   await page.waitForTimeout(1000);
+  page.on('pageerror', console.log);
   await browser.close();
 })();
